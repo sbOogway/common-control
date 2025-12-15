b0b04aa821e33bc039fe79e4dba3b5add3874e5a
Juice Man <92176188+sbOogway@users.noreply.github.com>
2025-12-15 16:40:49 +0100

    adding constant files

--- include/control-common/control-common.h
+++ include/control-common/control-common.h
@@ -2,3 +2,8 @@
 char const *exported_function(void);
 void write_pid_to_file(const char *pid_file);
 int get_pid_from_file(const char *pid_file);
+
+const char * CURRENT_TEMPERATURE_FILE  = "/opt/amel/current-temperature";
+const char * TARGET_TEMPERATURE_FILE   = "/opt/amel/target-temperature";
+const char * TEMP_CONTROL_PID_FILE     = "/opt/amel/temp-control-pid";
+const char * PID_CONTROL_PID_FILE      = "/opt/amel/pid-control-pid";
