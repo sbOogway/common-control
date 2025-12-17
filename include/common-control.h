7549571897871d3d0cbd8b28fe288ff6161a1e5a
Mattia Papaccioli <92176188+sbOogway@users.noreply.github.com>
2025-12-17 14:57:28 +0100

    refactor writing values to file

--- include/common-control.h
+++ include/common-control.h
@@ -1,10 +1,11 @@
 /* source/common-control.c */
-char const* exported_function(void);
-void write_pid_to_file(const char* pid_file);
 int get_pid_from_file(const char* pid_file);
-void write_float_to_file(char* file, float value);
+void write_float_to_file(const char* file, float value);
+void write_char_to_file(const char* file, char value);
+void write_pid_to_file(const char* pid_file);
 
-#define CURRENT_TEMPERATURE_FILE "/opt/amel/current-temperature"
-#define TARGET_TEMPERATURE_FILE "/opt/amel/target-temperature"
-#define TEMP_CONTROL_PID_FILE "/opt/amel/temp-control-pid"
-#define PID_CONTROL_PID_FILE "/opt/amel/pid-control-pid"
+#define CURRENT_TEMPERATURE_FILE    "/opt/amel/current-temperature"
+#define TARGET_TEMPERATURE_FILE     "/opt/amel/target-temperature"
+#define TEMP_CONTROL_PID_FILE       "/opt/amel/temp-control-pid"
+#define PID_CONTROL_PID_FILE        "/opt/amel/pid-control-pid"
+#define NUMBER_OF_SENSORS_FILE      "/opt/amel/number-sensors"
--- source/common-control.c
+++ source/common-control.c
@@ -1,25 +1,48 @@
-#include <pthread.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
 #include "common-control.h"
 
-char const* exported_function(void)
-{
-  return "common-control";
-}
+#include <errno.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-void write_pid_to_file(const char* pid_file)
+static int write_value_to_file(const char* filename, const char* format, ...)
 {
-  FILE* pid_fd = fopen(pid_file, "w");
-  if (!pid_fd) {
-    fprintf(stderr, "Failed to open PID file %s for writing\n", pid_file);
-    return;
+  if (!filename || !format) {
+    fprintf(stderr, "Error: NULL parameters passed to write_value_to_file\n");
+    return -1;
   }
-  fprintf(pid_fd, "%d\n", getpid());
-  fclose(pid_fd);
+
+  FILE* fd = fopen(filename, "w");
+  if (!fd) {
+    fprintf(stderr,
+            "Failed to open file %s for writing: %s\n",
+            filename,
+            strerror(errno));
+    return -1;
+  }
+
+  va_list args;
+  va_start(args, format);
+  int result = vfprintf(fd, format, args);
+  va_end(args);
+
+  if (result < 0) {
+    fprintf(
+        stderr, "Error writing to file %s: %s\n", filename, strerror(errno));
+    fclose(fd);
+    return -1;
+  }
+
+  if (fclose(fd) != 0) {
+    fprintf(stderr, "Error closing file %s: %s\n", filename, strerror(errno));
+    return -1;
+  }
+
+  return 0;
 }
 
 static int get_pid_from_file_single(const char* pid_file)
@@ -44,13 +67,23 @@ pid_t get_pid_from_file(const char* pid_file)
   return get_pid_from_file_single(pid_file);
 }
 
-void write_float_to_file(char* file, float value)
+void write_pid_to_file(const char* pid_file)
 {
-  FILE* current_temperature_fd = fopen(file, "w+");
-  char buf[8];
-  sprintf(buf, "%f\n", value);
-  if (fputs(buf, current_temperature_fd) == EOF) {
-    fprintf(stderr, "Error writing to file\n");
+  if (write_value_to_file(pid_file, "%d\n", getpid()) != 0) {
+    fprintf(stderr, "Failed to write PID to file %s\n", pid_file);
   }
-  fclose(current_temperature_fd);
+}
+
+void write_float_to_file(const char* file, float value)
+{
+  if (write_value_to_file(file, "%f\n", value) != 0) {
+    fprintf(stderr, "Failed to write float to file %s\n", file);
+  }
+}
+
+void write_char_to_file(const char* file, char value)
+{
+  if (write_value_to_file(file, "%d\n", value) != 0) {
+    fprintf(stderr, "Failed to write char to file %s\n", file);
+  }
 }
