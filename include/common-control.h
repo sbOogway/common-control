578f5769e18f10e31c8e81a8e2bc306999f22f70
Mattia Papaccioli <92176188+sbOogway@users.noreply.github.com>
2025-12-18 16:12:01 +0100

    refactor header file

--- include/common-control.h
+++ include/common-control.h
@@ -10,11 +10,14 @@ extern "C" {
 #  endif
 
 /* Existing functions */
+int get_int_from_file(const char* file);
 int get_pid_from_file(const char* pid_file);
+float get_float_from_file(const char* file);
 void write_float_to_file(const char* file, float value);
 void write_char_to_file(const char* file, char value);
 void write_pid_to_file(const char* pid_file);
 int sign(int value);
+
 
 #  ifdef __cplusplus
 }
@@ -22,10 +25,19 @@ int sign(int value);
 
 #endif /* COMMON_CONTROL_H */
 
-#define CURRENT_TEMPERATURE_FILE "/opt/amel/current-temperature"
-#define TARGET_TEMPERATURE_FILE "/opt/amel/target-temperature"
-#define TEMP_CONTROL_PID_FILE "/opt/amel/temp-control-pid"
-#define PID_CONTROL_PID_FILE "/opt/amel/pid-control-pid"
-#define NUMBER_OF_SENSORS_FILE "/opt/amel/number-sensors"
+#define ROOT_DIR "/opt/amel"
 
-#define SAMPLE_RATE 1 // in hertz
+#define PID_SUFFIX "-pid"
+
+#define TEMP_CONTROL_PROCESS_NAME "temp-control"
+#define PID_CONTROL_PROCESS_NAME  "pid-control"
+
+#define CURRENT_TEMPERATURE_FILE ROOT_DIR "/current-temperature"
+#define TARGET_TEMPERATURE_FILE  ROOT_DIR "/target-temperature"
+#define TEMP_CONTROL_PID_FILE    ROOT_DIR "/" TEMP_CONTROL_PROCESS_NAME PID_SUFFIX
+#define PID_CONTROL_PID_FILE     ROOT_DIR "/" PID_CONTROL_PROCESS_NAME  PID_SUFFIX
+#define NUMBER_OF_SENSORS_FILE   ROOT_DIR "/number-sensors"
+
+
+#define SAMPLE_RATE 1  // in hertz
+#define SERIAL_PORT "/dev/ttyUSB0"
