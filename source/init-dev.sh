691b1374a87b8bc891379721f52a0e8cf8112caa
Mattia Papaccioli <92176188+sbOogway@users.noreply.github.com>
2025-12-18 16:13:02 +0100

    adding script for local dev and modyfing run.sh

---
+++ source/init-dev.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+mkdir -p /opt/amel/pid/
+mkdir -p /opt/amel/temp-control/
+cp /home/oogway/amel/tesi-gui/build/bin/lvglsim /opt/amel/temp-control/main
+cp /home/oogway/amel/pid/pid /opt/amel/pid/main
--- source/run.sh
+++ source/run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+rm -f /opt/amel/pid-control-pid
+rm -f /opt/amel/temp-control-pid
+
 /opt/amel/pid/main &
 /opt/amel/temp-control/main &
