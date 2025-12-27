1e21fb3693d604b476adb2764a70b7668eccd531
Mattia Papaccioli <92176188+sbOogway@users.noreply.github.com>
2025-12-27 15:57:19 +0100

    refactor: splitting config.h from common-control.h

--- include/common-control.h
+++ include/common-control.h
@@ -1,13 +1,14 @@
 #ifndef COMMON_CONTROL_H
-#  define COMMON_CONTROL_H
+#define COMMON_CONTROL_H
 
-#  include <sys/types.h>
+#include <sys/types.h>
 
-#  include "logging.h"
+#include "logging.h"
+#include "config.h"
 
-#  ifdef __cplusplus
+#ifdef __cplusplus
 extern "C" {
-#  endif
+#endif
 
 /* Existing functions */
 int get_int_from_file(const char* file);
@@ -18,26 +19,8 @@ void write_char_to_file(const char* file, char value);
 void write_pid_to_file(const char* pid_file);
 int sign(int value);
 
-
-#  ifdef __cplusplus
+#ifdef __cplusplus
 }
-#  endif
+#endif
 
 #endif /* COMMON_CONTROL_H */
-
-#define ROOT_DIR "/opt/amel"
-
-#define PID_SUFFIX "-pid"
-
-#define TEMP_CONTROL_PROCESS_NAME "temp-control"
-#define PID_CONTROL_PROCESS_NAME  "pid-control"
-
-#define CURRENT_TEMPERATURE_FILE ROOT_DIR "/current-temperature"
-#define TARGET_TEMPERATURE_FILE  ROOT_DIR "/target-temperature"
-#define TEMP_CONTROL_PID_FILE    ROOT_DIR "/" TEMP_CONTROL_PROCESS_NAME PID_SUFFIX
-#define PID_CONTROL_PID_FILE     ROOT_DIR "/" PID_CONTROL_PROCESS_NAME  PID_SUFFIX
-#define NUMBER_OF_SENSORS_FILE   ROOT_DIR "/number-sensors"
-
-
-#define SAMPLE_RATE 1  // in hertz
-#define SERIAL_PORT "/dev/ttyUSB0"
