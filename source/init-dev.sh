8576d738f1ed8b0b64c812f1467bde7776a2e723
Mattia Papaccioli <92176188+sbOogway@users.noreply.github.com>
2026-01-07 21:41:01 +0100

    changing local build script

--- source/init-dev.sh
+++ source/init-dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-mkdir -p /opt/amel/pid/
-mkdir -p /opt/amel/temp-control/
-cp /home/oogway/amel/tesi-gui/build/bin/lvglsim /opt/amel/temp-control/main
-cp /home/oogway/amel/pid/pid /opt/amel/pid/main
+# mkdir -p /opt/amel/pid/
+# mkdir -p /opt/amel/temp-control/
+cp /home/oogway/amel/tesi-gui/build/bin/temp-control /opt/amel/temp-control
+cp /home/oogway/amel/pid/pid /opt/amel/pid-control
