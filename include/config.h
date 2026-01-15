f70c80e5d9b2dc1dc66bfce3a9447cdd87762603
Mattia Papaccioli <92176188+sbOogway@users.noreply.github.com>
2026-01-15 15:05:13 +0100

    adding new constants

--- include/config.h
+++ include/config.h
@@ -17,11 +17,19 @@
 #define NUMBER_OF_SENSORS_FILE   ROOT_DIR "/" NUMBER_OF_SENSORS
 
 
-#define MIN_TEMP 10.0
-#define MAX_TEMP 30.0
+#define MIN_OUTPUT 0.0
+#define MAX_OUTPUT 10000.0 // maximum value for the inverter is 20000
+
+#define KP 1.0
+#define KI 1.0
+#define KD 0.0
+
 #define DEFAULT_TARGET_TEMPERATURE 18.00
 
 
 #define SAMPLE_RATE 0.2 // in hertz
 #define SAMPLE_TIME  1.0/SAMPLE_RATE
-#define SERIAL_PORT "/dev/ttyUSB0"
+
+
+#define SERIAL_PORT_INVERTER "/dev/ttyUSB0"
+#define SERIAL_PORT_SENSORS  "/dev/ttyUSB1"
